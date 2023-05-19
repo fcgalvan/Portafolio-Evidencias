95ef829bad2cf2f7d0282af5d23f22c5668c0a79
fcgalvan <125666519+fcgalvan@users.noreply.github.com>
2023-05-19 07:55:57 -0600

    Add files via upload

---
+++ examenpracticop3.js
@@ -0,0 +1,10 @@
+function descuento(){
+    var mes = 05;
+    var fecha = document.getElementsByName(fechan);
+    if(fecha == mes){
+        <p>Felicidades, este mes es tu cumpleaños!! Obtén 10% de descuento!</p>
+
+    }else
+    <p>Tu mes de nacimiento no tiene un descuento por el momento</p>
+      
+}
